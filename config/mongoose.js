646454cc84244c265049749161972e88c2eae448
akshatspeaking <akshatspeaking@gmail.com>
2020-08-17 14:47:09 +0530

    initial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,14 @@
 var mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/conduit", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    "mongodb+srv://akshatspeaking:conduitpassword@conduit-api.i43oi.mongodb.net/conduit?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
   .then((result) => {
     console.log("Connected to DB");
   })
